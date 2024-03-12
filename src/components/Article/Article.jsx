94500ac640430f3c6510ce957d06f83ffed298d8
mstent <mlstenton@gmail.com>
2024-03-12 15:56:53 +0000

    add ability to view individual articles

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,13 +1,10 @@
-import { useSearchParams } from "react-router-dom";
-import ArticleCard from "../ArticleCard.jsx/ArticleCard";
+import ArticleCard from "../ArticleCard/ArticleCard";
 import CommentsList from "../CommentsList/CommentsList";
 
 const Article = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
 
     return (
         <div>
-            Article
             <ArticleCard />
             <CommentsList />
         </div>
