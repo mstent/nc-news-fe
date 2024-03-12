f5c2c43b17cb44b9a916202e14d8909e36171afb
mstent <mlstenton@gmail.com>
2024-03-12 10:23:53 +0000

    add listing of all articles on main page

---
+++ src/utils/dateConverter.js
@@ -0,0 +1,9 @@
+const dateConverter = (date) => {
+    const year = date.slice(0, 4)
+    const month = date.slice(5, 7)
+    const day = date.slice(8, 10)
+
+    return `${day}/${month}/${year}`
+}
+
+export default dateConverter
