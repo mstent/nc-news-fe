9a69dffd42b73797e817ea60ce9f683d5661557b
mstent <mlstenton@gmail.com>
2024-03-13 11:23:32 +0000

    add list of comments on each article page

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,8 +1,35 @@
-const CommentCard = () => {
+import styles from "./CommentCard.module.css"
+import dateConverter from "../../utils/dateConverter";
+import { useEffect, useState } from "react";
+import { getUserInfo } from "../../utils/apis";
+
+const CommentCard = ({comment}) => {
+    const [commentAuthorInfo, setCommentAuthorInfo] = useState({});
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        setIsLoading(true)
+        getUserInfo(comment.author).then((userInfo) => {
+            setCommentAuthorInfo(userInfo);
+            setIsLoading(false);
+        })
+    }, [])
+
     return (
-        <div>
-            CommentCard
-        </div>
+        <li className={styles["comment-card-container"]}>
+            <div className={styles["comment-author-container"]}>
+                {isLoading ? <p className={styles["loading-avatar"]}>Loading {comment.author} avatar...</p> : <>
+                <img src={commentAuthorInfo.avatar_url} className={styles["comment-user-avatar"]}/>
+                <p className={styles["comment-user-username"]}>{commentAuthorInfo.username}</p></>
+                }
+
+            </div>
+            <div className={styles["comment-info-container"]}>
+                <div className={styles["comment-date-delete-container"]}>
+                    <p className={styles["date-made"]}>{dateConverter(String(comment.created_at))}</p>
+                </div>
+                <p className={styles["comment-body"]}>{comment.body}</p>
+            </div>
+        </li>
     );
 };
 
--- src/components/CommentsList/CommentsList.jsx
+++ src/components/CommentsList/CommentsList.jsx
@@ -1,12 +1,43 @@
 import CommentCard from "../CommentCard/CommentCard";
-import PostCommentForm from "../PostCommentForm/PostCommentForm"
+import PostCommentForm from "../PostCommentForm/PostCommentForm";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getComments } from "../../utils/apis";
+import styles from "./CommentsList.module.css";
 
 const CommentsList = () => {
+    const [commentsListArr, setCommentsListArr] = useState([]);
+    const { article_id } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        getComments(article_id).then((comments) => {
+            setCommentsListArr(comments);
+            setIsLoading(false);
+        });
+    }, []);
+
     return (
         <div>
             CommentsList
-            <PostCommentForm />
-            <CommentCard />
+            {isLoading ? (
+                <p className={styles["loading"]}>Loading Comments...</p>
+            ) : (
+                <>
+                    <PostCommentForm />
+                    <h2 className={styles["comments-header"]}>Comments</h2>
+                    <ul className={styles["comments-list-container"]}>
+                        {commentsListArr.map((comment) => {
+                            return (
+                                <CommentCard
+                                    comment={comment}
+                                key={comment.comment_id}/>
+                            );
+                        })}
+                    </ul>
+                </>
+            )}
         </div>
     );
 };
