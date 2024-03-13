84f4385edc3e011b75044dd8576ca08ec4c23009
mstent <mlstenton@gmail.com>
2024-03-13 09:10:18 +0000

    add loading state to article card and article list cards

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -2,45 +2,56 @@ import styles from "./ArticleCard.module.css";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getArticleById, getUserInfo } from "../../utils/apis";
-import dateConverter from '../../utils/dateConverter';
+import dateConverter from "../../utils/dateConverter";
 
 const ArticleCard = () => {
     const [article, setArticle] = useState({});
     const [authorInfo, setAuthorInfo] = useState({});
     const { article_id } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
+        setIsLoading(true);
         getArticleById(article_id).then((article) => {
             setArticle(article);
             getUserInfo(article.author).then((authorInfo) => {
                 setAuthorInfo(authorInfo);
+                setIsLoading(false);
             });
         });
     }, []);
 
     return (
-        <div className={styles["article-card-container"]}>
-            <div className={styles["article-card-author-info"]}>
-                <img
-                    src={authorInfo.avatar_url}
-                    alt=""
-                    className={styles["article-author-avatar"]}
-                />
-                <h3 className={styles["article-card-author"]}>
-                    {article.author}
-                </h3>
-            </div>
-                <h2 className={styles["article-card-title"]}>
-                    {article.title}
-                </h2>
-            <p className={styles["article-card-body"]}>{article.body}</p>
-            <div className={styles["article-card-vote-count"]}>
-                Votes: {article.votes}
-            </div>
-            <div className={styles["article-card-date"]}>
-                {dateConverter(String(article.created_at))}
-            </div>
-        </div>
+        <>
+            {isLoading ? (
+                <p className={styles["loading"]}>Loading Article...</p>
+            ) : (
+                <div className={styles["article-card-container"]}>
+                    <div className={styles["article-card-author-info"]}>
+                        <img
+                            src={authorInfo.avatar_url}
+                            alt=""
+                            className={styles["article-author-avatar"]}
+                        />
+                        <h3 className={styles["article-card-author"]}>
+                            {article.author}
+                        </h3>
+                    </div>
+                    <h2 className={styles["article-card-title"]}>
+                        {article.title}
+                    </h2>
+                    <p className={styles["article-card-body"]}>
+                        {article.body}
+                    </p>
+                    <div className={styles["article-card-vote-count"]}>
+                        Votes: {article.votes}
+                    </div>
+                    <div className={styles["article-card-date"]}>
+                        {dateConverter(String(article.created_at))}
+                    </div>
+                </div>
+            )}
+        </>
     );
 };
 
